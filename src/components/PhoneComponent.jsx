7aa63c7c06faef89efbc33558d9410f708b677d1
Stanislas-Web <stanislasmakengo1@gmail.com>
2022-07-07 14:16:10 +0100

    react redux

---
+++ src/components/PhoneComponent.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { connect } from "react-redux";
+import phone from "../images/phone.jpg";
+import { buyPhone } from '../redux/phone/actionPhone';
+
+const PhoneComponent = (props) => {
+
+  console.log(props);
+  return (
+      <div className="child">
+        <img
+          width="300"
+          height="200"
+          src={phone}
+          alt="phone"
+        />
+        <p>
+          Disponibilité : <span id="count">{props.phones}</span>
+        </p>
+        <button onClick={()=> props.buyPhone()} id="buy-phone">Achetter</button>
+      </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    phones: state.phones
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyPhone: () => dispatch(buyPhone())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux';
+
+import phoneReducer from './phone/reducerPhone';
+
+
+
+const store = createStore(phoneReducer);
+
+export default store; 
